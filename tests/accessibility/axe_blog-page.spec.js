e2a037458b381250df4ac927da224dabb286726c
Pablo Vergara <vergara.pablo0616@gmail.com>
2023-12-11 16:42:07 -0500

    accessibility tests for shopping by manufacturer .. buggy section

--- tests/accessibility/axe_blog-page.spec.js
+++ tests/accessibility/axe_blog-page.spec.js
@@ -5,7 +5,7 @@ const { baseURL }  = require('../test-data/data')
 const AxeBuilder = require('@axe-core/playwright').default
 
 test.describe('@accessibility', () => {
-    test('Accessibility Test | Shop By Category - Components PDP', async ({page}) => {
+    test('Accessibility Test | Blog Section', async ({page}) => {
         await page.goto(baseURL+'?route=extension/maza/blog/home');
         const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); 
         expect(accessibilityScanResults.violations).toEqual([]);
